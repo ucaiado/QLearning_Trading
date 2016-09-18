63ba9013ddf5d959419925474db34ea3b30801bf
ucaiado <ucaiado@yahoo.com.br>
2016-09-18 16:49:57 -0300

    docs: write about metrics

---
+++ qtrader/eda.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Perform simple statistical data analysis in log files produced by simulation
+
+@author: ucaiado
+
+Created on 09/18/2016
+"""
+from collections import defaultdict
+import pandas as pd
